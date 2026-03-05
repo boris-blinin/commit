561f530f2511d33c8855b3fe528292e9b282562c
Alberto <albertaihin747@gmail.com>
2026-03-06 01:31:09 +0500

    Добавление маршрутов

--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,6 @@ use App\Http\Controllers\IssueController;
 use App\Http\Controllers\ItemController;
 use Illuminate\Support\Facades\Route;
 
+
 Route::get('/', [IssueController::class, 'index'])->name('home');
 Route::resource('issues', IssueController::class)->except(['show']);
